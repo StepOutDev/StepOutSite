bb7d6f4d671d7e92604b290b4c17452952568006
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2024-11-19 20:51:30 +0700

    my profile without edit

--- frontend/src/app/myprofile/page.tsx
+++ frontend/src/app/myprofile/page.tsx
@@ -19,12 +19,12 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#E8DEF8]"
-        style={{
+    <div className="flex flex-col items-center justify-center h-screen bg-[#E8DEF8]">
+        <h1 style={{
             fontFamily: '"Poppins", sans-serif', // Closest match to "Poppy"
             textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', // Custom shadow effect
-        }}>
-        <h1 className="flex items-center justify-center w-full text-[75px] font-extrabold absolute top-[90px] text-[#7F62AC] drop-shadow-md transition-transform transform ">My Profile</h1>{/*hover:scale-105*/}
+            }}
+            className="flex items-center justify-center w-full text-[75px] font-extrabold absolute top-[90px] text-[#7F62AC] drop-shadow-md transition-transform transform ">My Profile</h1>{/*hover:scale-105*/}
         {/* Profile Picture */}
         <img
             src={userProfile.profilePic}
@@ -37,23 +37,21 @@ const ProfilePage = () => {
       <div className='rounded-3xl bg-[#F2D3EF] flex-col flex items-center w-[900px] h-[300px] mx-auto p-6 text-[#184A92] relative shadow-2xl absolute top-[185px]'>
 
         {/* Name */}
-        <h1 className="flex items-center justify-center w-full text-2xl font-bold absolute top-[15px]">{userProfile.knickname}</h1>
-        <div className='flex flex-row absolute w-full absolute top-[55px]'>
-            <div className='flex flex-col w-[500px] h-[170px] absolute left-[50px] top-[0px] justify-between h-full'>
+        <h1 className="flex items-center justify-center w-full text-2xl font-bold absolute top-[7px]">{userProfile.knickname}</h1>
+        <div className='flex flex-row absolute w-full absolute top-[53px]'>
+            <div className='flex flex-col w-[500px] h-[180px] absolute left-[50px] top-[0px] justify-between h-full'>
                 <h1 className=" w-full text-2xl font-bold">Full Name : {userProfile.firstlastname}</h1>
                 <h1 className="w-full text-2xl font-bold">Year : {userProfile.Year}</h1>
                 <h1 className="w-full text-2xl font-bold items-center">Role : {userProfile.Role}</h1>
                 <h1 className="w-full block text-2xl font-bold items-center">Tel : {userProfile.Tel}</h1>
             </div>
-            <div className='flex flex-col w-[300px] h-[170px] absolute left-[570px] top-[0px] justify-between h-full'>
+            <div className='flex flex-col w-[300px] h-[180px] absolute left-[570px] top-[0px] justify-between h-full'>
                 <h1 className=" w-full text-2xl font-bold">StudentId : {userProfile.StudentId}</h1>
                 <h1 className="w-full text-2xl font-bold">Major : {userProfile.Major}</h1>
                 <h1 className="w-full text-2xl font-bold items-center">IG : {userProfile.IG}</h1>
                 <h1 className="w-full block text-2xl font-bold items-center">LineID {userProfile.LineID}</h1>
             </div>
         </div>
-
-        
 
         {/* <h1 className="flex items-center justify-center w-full text-2xl font-bold mt-12">{userProfile.name}</h1> */}
         
