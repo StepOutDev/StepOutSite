76bf038ca32a55f4a1569cf64a6bd061e40f1c85
NattapatDaengkongkaew <nattapatque.daengkongkaew@gmail.com>
2024-11-19 21:17:01 +0700

    topmenu without session and contact

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 import Banner from "@/components/bannerComponents/banner";
+import Contact from "@/components/contactComponents/contact";
 
 export default function Home() {
   return (
     <main className="absolute inset-0 z-10 w-full"> 
-      <Banner/>
+      <Banner></Banner>
 
       <div className="mt-[5%] ml-[10%]  text-[#422A40] text-[32px] font-bold text-poppins ">
         Upcoming Event
       </div>
 
+      <Contact></Contact>
     </main>
   )
 }
