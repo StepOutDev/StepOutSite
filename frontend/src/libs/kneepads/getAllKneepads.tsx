78b6f1ff87cf314aac066d456bd674e73d39b430
kawinwat <kawinwatleo48@gmail.com>
2025-02-22 11:47:31 +0700

    feat: libs get all kneepads

---
+++ frontend/src/libs/kneepads/getAllKneepads.tsx
@@ -0,0 +1,16 @@
+export default async function GetAllKneepads(token: string) {
+    const response = await fetch(
+      `http://127.0.0.1:5000/api/v1/kneepads/get_all`,
+      {
+        method: "GET",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Cannot get all kneepads")
+    };
+  
+    return await response.json();
+  }
