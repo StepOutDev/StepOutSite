a41ddf396ac2e9da81d7916a0778900bcab95c60
kawinwat <kawinwatleo48@gmail.com>
2025-02-22 11:50:36 +0700

    feat: libs update kneepads

---
+++ frontend/src/libs/kneepads/updateKneepads.tsx
@@ -0,0 +1,19 @@
+import { Kneepads } from "../../../interface";
+
+export default async function UpdateKneepads(kneepad: Kneepads, number: string, token: string) {
+    const response = await fetch(
+      `http://127.0.0.1:5000/api/v1/kneepads/update/${number}`,
+      {
+        method: "PUT",
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(kneepad),
+      }
+    );
+    if (!response.ok) {
+      throw new Error("Cannot update kneepads")
+    };
+  
+    return await response.json();
+  }
