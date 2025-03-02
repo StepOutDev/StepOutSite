9ea33c2cb6c2d550f782877ba8ef5ffaaf5f0712
NattapatDaengkongkaew <nattapatque.daengkongkaew@gmail.com>
2025-03-02 21:18:29 +0700

    implement: manage event page and panel

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import Banner from "@/components/bannerComponents/banner";
 import Contact from "@/components/contactComponents/contact";
-import { useSession } from "next-auth/react";
 import UpcomingEvent from "@/components/eventComponents/upcomingEvent";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="absolute inset-0 z-10 w-full"> 
       <Banner></Banner>
-
+      
       <UpcomingEvent/>
 
       <Contact></Contact>
--- frontend/src/components/eventComponents/upcomingEvent.tsx
+++ frontend/src/components/eventComponents/upcomingEvent.tsx
@@ -1,27 +1,20 @@
-"use client"
-
-import EventCard from "./eventCard"
+import Link from "next/link"
+import EventPanel from "./EventPanel"
 
 export default function UpcomingEvent(){
-    return (
+    return(
         <div>
             <div className="flex md:flex-row flex-col justify-between items-center mt-[5%] mx-[10%] space-y-3">
                 <div className="text-[#422A40] text-[32px] font-bold">
                     Upcoming Event
                 </div>
-                <button
-                    
+                <Link
+                    href={'/eventmanage'}
                     className="px-8 py-2 bg-white rounded-lg border-2 border-[#ED79B7] text-[#ED79B7] text-[16px]">
                     manage event
-                </button>
+                </Link>
             </div>
-            <div className="flex space-x-4 w-[80%] h-[600px] overflow-x-scroll mt-[10px] mx-auto text-black
-                custom-scrollbar">
-                <EventCard/>
-                <EventCard/>
-                <EventCard/>
-                <EventCard/>
-            </div>
+            <EventPanel/>
         </div>
     )
 }
