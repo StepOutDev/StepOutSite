69b8cb5aab2b05186e5fa688ba3e5333d1a6a194
LAPTOP-ISEKNL7Q\Asus <janny20.1.2549@gmail.com>
2025-03-17 15:52:44 +0700

    my profile complete

--- frontend/src/app/myprofile/page.tsx
+++ frontend/src/app/myprofile/page.tsx
@@ -17,7 +17,7 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogTitle from '@mui/material/DialogTitle';
 import TextField from '@mui/material/TextField';
-import { MenuItem, FormControl, InputLabel, Select } from "@mui/material";
+import { MenuItem, FormControl, InputLabel, Select, SelectChangeEvent } from "@mui/material";
 
 
 import ProfileImg from '@/components/profile/profileImg';
@@ -118,7 +118,7 @@ const MyProfilePage = () => {
     }
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement> | SelectChangeEvent<string>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement> | SelectChangeEvent<string>) => {
 
     const { name, value } = e.target;
 
@@ -180,163 +180,197 @@ const MyProfilePage = () => {
   
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[#E8DEF8]">
-      <h1
-        style={{
-          fontFamily: '"Poppins", sans-serif', // Closest match to "Poppy"
-          textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', // Custom shadow effect
-        }}
-        className="flex items-center justify-center w-full text-[75px] font-extrabold absolute top-[90px] text-[#7F62AC] drop-shadow-md transition-transform transform"
-      >
-        My Profile
-      </h1>
-      
-
-      <div className="rounded-3xl bg-[#F2D3EF] flex flex-row items-center w-[1400px] h-[300px] mx-auto p-6 text-[#184A92] relative shadow-2xl absolute top-[100px]"> {/*absolute top-[220px]*/}
-
+    <div className=''>
+      <div className="min-h-screen flex flex-col bg-[#E8DEF8]">
+        <h1
+          style={{
+            fontFamily: '"Poppins", sans-serif', // Closest match to "Poppy"
+            textShadow: '2px 2px 5px rgba(0, 0, 0, 0.3)', // Custom shadow effect
+          }}
+          className="flex mt-10 items-center justify-center w-full text-[75px] font-extrabold pt-10 text-[#7F62AC] drop-shadow-md transition-transform transform"
+        >
+          My Profile
+        </h1>
         
-        <div className="flex flex-row absolute w-full absolute top-[53px]">{/*absolute top-[7px]*/}
-          <div className='flex flex-row basis-1/2'>
-            <div className='flex basis-1/3'><ProfileImg imageUrl={profile?.image} handleImageUpload={handleImageUpload} /></div>
-            <div className="flex flex-col justify-end mb-10">{/*flex flex-col w-[500px] h-[180px] absolute left-[50px] top-[0px] justify-between h-full*/}
-              <div className="flex flex-row gap-4 mb-6">
-                <h1 className="text-4xl font-bold">{profile?.first_name} {profile?.last_name} ({profile?.nick_name})</h1>
-                <h1 className="text-[15px] rounded-full px-6 h-6 mt-2 font-bold flex items-center bg-[#FFEDD5]">{profile?.role}</h1>
+
+        <div className="justify-items-center md:px-10 px-5">{/*rounded-3xl bg-[#FFE3EF] flex md:flex-row flex-col items-center w-[1400px] h-[400px] mx-auto p-6 text-[#184A92] relative shadow-2xl absolute top-[100px] absolute top-[220px]*/}
+
+          
+          <div className="w-full mx-8 rounded-3xl flex md:flex-row flex-col p-6 text-[#184A92] md:relative shadow-2xl my-10 bg-[#FFE3EF] ">{/*absolute top-[7px]*/}
+              <div className='flex basis-1/6 mx:ml-10 mb-10 justify-center'><ProfileImg imageUrl={profile?.image} handleImageUpload={handleImageUpload} /></div>
+              <div className="flex flex-col md:mb-10 md:ml-10">{/*flex flex-col w-[500px] h-[180px] absolute left-[50px] top-[0px] justify-between h-full*/}
+                <div className="flex md:flex-row gap-4 mb-6">
+                  <div className='flex flex-col md:gap-4 gap-2'>
+                    <div className='flex md:flex-row flex-col md:gap-4 md:max-w-[500px] max-w-[250px] gap-2'>
+                      <h1 className="text-4xl font-bold overflow-x-auto">{profile?.first_name} </h1>
+                      <h1 className="text-4xl font-bold overflow-x-auto">{profile?.last_name} </h1>
+                    </div>
+                    <div><h1 className="text-4xl font-bold">({profile?.nick_name})</h1></div>
+                  </div>
+                  <h1 className="text-[15px] rounded-full px-6 h-6 mt-2 font-bold flex md:items-center bg-[#FFDC94]">{profile?.role}</h1>
+                </div>
+                <div className="w-full h-[2px] md:bg-[#FFE3EF] bg-black my-4" />
+                <div className='md:h-[200px] leading-[3rem]'>
+                  <div className='flex flex-row '>
+                    <h1 className="md:w-[200px] w-[400px] text-[20px] font-bold">StudentId</h1>
+                    <h1 className=" w-full text-[20px] font-bold"> {profile?.student_id} </h1>
+                  </div>
+                  <div className='flex flex-row'>
+                    <h1 className="md:w-[200px] w-[400px] text-[20px] font-bold">Year</h1>
+                    <h1 className=" w-full text-[20px] font-bold"> {profile?.year} </h1>
+                  </div>
+                  <div className='flex flex-row'>
+                    <h1 className="md:w-[200px] w-[400px] text-[20px] font-bold">Major</h1>
+                    <h1 className=" w-full text-[20px] font-bold"> {profile?.major} </h1>
+                  </div>
+                </div>
               </div>
-              <h1 className=" w-full text-[20px] font-bold">StudentId : {profile?.student_id} </h1>
-              <h1 className="w-full text-[20px] font-bold">Year : {profile?.year}</h1>
-              <h1 className="w-full text-[20px] font-bold">Major : {profile?.major}</h1>
+    
+            <div className="basis-1/3 flex flex-col justify-end mb-10 md:ml-20">{/*flex flex-col w-[300px] h-[180px] absolute left-[570px] top-[0px] justify-between h-full*/}
+              <div className='h-[200px] leading-[3rem]'>
+                <div className='flex flex-row'>
+                  <h1 className="md:w-[150px] w-[200px] text-[20px] font-bold items-center">Tel</h1>
+                  <h1 className="text-[20px] font-bold items-center">{profile?.telephone}</h1>  
+                </div>
+                <div className='flex flex-row'>
+                  <h1 className="md:w-[150px] w-[200px] text-[20px] font-bold items-center">IG</h1>
+                  <h1 className="text-[20px] font-bold items-center">{profile?.instagram}</h1>
+                </div>
+                <div className='flex flex-row'>
+                  <h1 className="md:w-[150px] w-[200px] text-[20px] font-bold items-center">LineID</h1>
+                  <h1 className="text-[20px] font-bold items-center">{profile?.line}</h1>  
+                </div>
+              </div>
+            </div>
+            
+            <div className='flex items-center justify-center'>
+              <Button
+                variant="outlined"
+                onClick={handleClickOpen}
+                sx={{
+                  width: 120,
+                  border: '2px solid', // Thicker border (3px)
+                  borderColor: '#184A92', // Custom border color
+                  borderRadius: '10px', // Makes the button rounder
+                  fontWeight: 'bold',
+                  '&:hover': {
+                    borderColor: '#1D3557', // Custom hover border color
+                  },
+                }}
+                className="mt-6 flex bg-[#F5F5F5] text-[#184A92] hover:bg-gray-300 md:absolute md:right-[100px]  md:top-[10px] text-[13px]"
+              >
+                Edit Profile
+              </Button>
             </div>
           </div>
-          <div className="basis-1/3 flex flex-col justify-end mb-10">{/*flex flex-col w-[300px] h-[180px] absolute left-[570px] top-[0px] justify-between h-full*/}
-            <h1 className="w-full block text-[20px] font-bold items-center">Tel : {profile?.telephone}</h1>
-            <h1 className="w-full text-[20px] font-bold items-center">IG : {profile?.instagram}</h1>
-            <h1 className="w-full block text-[20px] font-bold items-center">LineID : {profile?.line}</h1>
-          </div>
-          <Button
-            variant="outlined"
-            onClick={handleClickOpen}
-            sx={{
-              width: 120,
-              border: '2px solid', // Thicker border (3px)
-              borderColor: '#184A92', // Custom border color
-              borderRadius: '10px', // Makes the button rounder
-              fontWeight: 'bold',
-              '&:hover': {
-                borderColor: '#1D3557', // Custom hover border color
-              },
-            }}
-            className="flex items-center justify-center bg-[#F5F5F5] text-[#184A92] hover:bg-gray-300 absolute right-[100px] top-[10px] text-[13px]"
-          >
-            Edit Profile
-          </Button>
+
+          
+
         </div>
 
-        
-
-      </div>
-
-      {/* Edit Profile Modal */}
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Edit Profile</DialogTitle>
-        <DialogContent>
-          <TextField
-            label="First Name"
-            name="first_name"
-            value={editedProfile?.first_name || ""}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Last Name"
-            name="last_name"
-            value={editedProfile?.last_name || ""}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          {/* <TextField
-            label="Year"
-            name="year"
-            value={editedProfile?.year}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          /> */}
-          <FormControl fullWidth margin="normal">
-            <InputLabel>Year</InputLabel>
-            <Select
-              name="year"
-              value={editedProfile?.year || ""}
+        {/* Edit Profile Modal */}
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Edit Profile</DialogTitle>
+          <DialogContent>
+            <TextField
+              label="First Name"
+              name="first_name"
+              value={editedProfile?.first_name || ""}
               onChange={handleInputChange}
-            >
-              <MenuItem value="1">Year 1</MenuItem>
-              <MenuItem value="2">Year 2</MenuItem>
-              <MenuItem value="3">Year 3</MenuItem>
-              <MenuItem value="4">Year 4</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            label="Role"
-            name="role"
-            value={editedProfile?.role}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Tel"
-            name="telephone"
-            value={editedProfile?.telephone}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          {/* <TextField
-            label="Student ID"
-            name="StudentId"
-            value={editedProfile?student_id}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          /> */}
-          <TextField
-            label="Major"
-            name="major"
-            value={editedProfile?.major}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Instagram"
-            name="instagram"
-            value={editedProfile?.instagram}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-          <TextField
-            label="Line ID"
-            name="line"
-            value={editedProfile?.line}
-            onChange={handleInputChange}
-            fullWidth
-            margin="normal"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSave} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
-      
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Last Name"
+              name="last_name"
+              value={editedProfile?.last_name || ""}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            {/* <TextField
+              label="Year"
+              name="year"
+              value={editedProfile?.year}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            /> */}
+            <FormControl fullWidth margin="normal">
+              <InputLabel>Year</InputLabel>
+              <Select
+                name="year"
+                value={editedProfile?.year || ""}
+                onChange={handleInputChange}
+              >
+                <MenuItem value="1">Year 1</MenuItem>
+                <MenuItem value="2">Year 2</MenuItem>
+                <MenuItem value="3">Year 3</MenuItem>
+                <MenuItem value="4">Year 4</MenuItem>
+              </Select>
+            </FormControl>
+            <TextField
+              label="Role"
+              name="role"
+              value={editedProfile?.role}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Tel"
+              name="telephone"
+              value={editedProfile?.telephone}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            {/* <TextField
+              label="Student ID"
+              name="StudentId"
+              value={editedProfile?student_id}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            /> */}
+            <TextField
+              label="Major"
+              name="major"
+              value={editedProfile?.major}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Instagram"
+              name="instagram"
+              value={editedProfile?.instagram}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Line ID"
+              name="line"
+              value={editedProfile?.line}
+              onChange={handleInputChange}
+              fullWidth
+              margin="normal"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleSave} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+        
+      </div>
     </div>
+
     
   );
 };
